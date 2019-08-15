9d9adf1cf120ee47ffbc02ea9222a5cd385739a1
serviktor050 <mr.s-gagulin@yandex.ru>
2019-08-15 22:33:37 +0300

    Добавлено решение 1 задачи

---
+++ element-search/popups/task.js
@@ -0,0 +1,22 @@
+const form = document.getElementById("modal_main");
+form.classList.add("modal_active");
+
+const closeForm = document.getElementsByClassName("modal__close")
+const firstCloseFormElement = closeForm.item(0);
+firstCloseFormElement.onclick = function() {
+    form.classList.remove("modal_active");
+}
+
+const changeButton = document.querySelector(".show-success");
+const changedButton = document.getElementById("modal_success");
+changeButton.onclick = function() {
+    changedButton.classList.add("modal_active");
+}
+
+const closeFormSecond = document.getElementsByClassName("modal__close_times")
+const secondCloseFormElement = closeFormSecond.item(1);
+
+secondCloseFormElement.onclick = function() {
+    form.classList.remove("modal_active");
+    changedButton.classList.remove("modal_active");
+}
