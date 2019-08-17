ca88b45f2053f09ac121d32cefe8ac5315ae3867
serviktor050 <mr.s-gagulin@yandex.ru>
2019-08-17 20:36:01 +0300

    Добавлено решение 3 задачи

---
+++ element-search/slider/task.js
@@ -0,0 +1,37 @@
+const prevButton = document.querySelector(".slider__arrow_prev");
+const nextButton = document.querySelector(".slider__arrow_next");
+
+let slideNumber = 0;
+
+const sliderImages = document.getElementsByClassName("slider__item");
+let sliderImage = sliderImages.item(slideNumber);
+
+nextButton.onclick = function() {
+    if (slideNumber >= 0 && slideNumber < sliderImages.length - 1) {
+        slideNumber++;
+        sliderImage = sliderImages.item(slideNumber);
+        sliderImage.classList.add("slider__item_active");
+    }else {
+        slideNumber = 0;
+        for (let i = 1; i < 5; i++) {
+            sliderImage = sliderImages.item(slideNumber + i);
+            sliderImage.classList.remove("slider__item_active");
+        }
+    }
+}
+
+prevButton.onclick = function() {
+    if (slideNumber <= sliderImages.length && slideNumber > 0 ) {
+        slideNumber--;
+        sliderImage = sliderImages.item(slideNumber + 1);
+        sliderImage.classList.remove("slider__item_active");
+        sliderImage = sliderImages.item(slideNumber);
+        sliderImage.classList.add("slider__item_active");
+    }else {
+        slideNumber = sliderImages.length - 1;
+        for (let i = 1; i < sliderImages.length; i--) {
+            sliderImage = sliderImages.item(slideNumber - i);
+            sliderImage.classList.add("slider__item_active");
+        }
+    }         
+}
