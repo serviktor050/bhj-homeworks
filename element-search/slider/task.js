3ee46ee6265eab72e1cc0257cd4444d17a578732
serviktor050 <mr.s-gagulin@yandex.ru>
2019-08-18 19:25:33 +0300

    Внесены корректировки во 3 задачу

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -9,29 +9,33 @@ let sliderImage = sliderImages.item(slideNumber);
 nextButton.onclick = function() {
     if (slideNumber >= 0 && slideNumber < sliderImages.length - 1) {
         slideNumber++;
+        sliderImage = sliderImages.item(slideNumber - 1);
+        sliderImage.classList.remove("slider__item_active");
         sliderImage = sliderImages.item(slideNumber);
         sliderImage.classList.add("slider__item_active");
     }else {
         slideNumber = 0;
+        sliderImage = sliderImages.item(slideNumber);
+        sliderImage.classList.add("slider__item_active");
         for (let i = 1; i < 5; i++) {
             sliderImage = sliderImages.item(slideNumber + i);
-            sliderImage.classList.remove("slider__item_active");
+            sliderImage.classList.remove("slider__item_active");            
         }
     }
 }
 
 prevButton.onclick = function() {
-    if (slideNumber <= sliderImages.length && slideNumber > 0 ) {
+    if (slideNumber < sliderImages.length && slideNumber > 0 ) {
         slideNumber--;
         sliderImage = sliderImages.item(slideNumber + 1);
         sliderImage.classList.remove("slider__item_active");
         sliderImage = sliderImages.item(slideNumber);
         sliderImage.classList.add("slider__item_active");
     }else {
-        slideNumber = sliderImages.length - 1;
-        for (let i = 1; i < sliderImages.length; i--) {
-            sliderImage = sliderImages.item(slideNumber - i);
-            sliderImage.classList.add("slider__item_active");
-        }
+        slideNumber = (sliderImages.length - 1);
+        sliderImage = sliderImages.item(0);
+        sliderImage.classList.remove("slider__item_active");
+        sliderImage = sliderImages.item(slideNumber);
+        sliderImage.classList.add("slider__item_active");        
     }         
 }
