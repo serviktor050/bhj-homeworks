9342141c877c7e3fdef575d87c4d3f2e3139a8c0
serviktor050 <mr.s-gagulin@yandex.ru>
2019-08-20 21:33:18 +0300

    Загрузка пробного решения задачи 2

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,3 +1,30 @@
+const viewSubMenu = document.getElementsByClassName("menu_sub"); // Свернутый список
+const changeButton = document.getElementsByClassName("menu__link"); //Анкор ссылки (кнопка)
+let menuItem = document.querySelectorAll("ul.menu.menu_sub");
+
+for (let i = 0; i < changeButton.length; i++) { //запускаем цикл по пунктам меню (выбранные кнопки)
+    let changedButton = changeButton.item(i);
+    changedButton.onclick = function() { //запускаем функцию на кнопке
+        for (let m = 0; m < viewSubMenu.length; m++) {
+            if (document.changeButton(i).nextElementSibling.includes("menu_sub") === true) { //содержит ли элемент сосед необходимый класс
+            viewSubMenu.item(m).classList.add("menu_active");
+            return false
+           }   
+        }      
+    }
+}
+
+/*viewSubMenu.item(m).classList.add("menu_active");
+                return false
+    /*viewSubMenu.item(1).classList.add("menu_active");
+        return false
+         {
+            viewSubMenu.item(m).classList.add("menu_active");
+            return false
+        } */  
+
+/*-----------------------------------------------------------*/
+/*
 const viewSubMenu = document.getElementsByClassName("menu_sub");
 
 const aboutCompanyViewSubMenu = viewSubMenu.item(0);
@@ -18,3 +45,4 @@ serviceChangedButton.onclick = function() {
     serviceViewSubMenu.classList.add("menu_active");
     return false
 }
+*/
