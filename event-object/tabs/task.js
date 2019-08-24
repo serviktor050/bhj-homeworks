5415857f0e7aa58a672cb5da7a461e7048ade287
serviktor050 <mr.s-gagulin@yandex.ru>
2019-08-24 13:32:00 +0300

    Скорректировано решение 3 задачи

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,34 +1,13 @@
 const menuItems = document.querySelectorAll(".tab");
 const contentFields = document.querySelectorAll(".tab__content")
 
-for (let i = 0; i < menuItems.length; i++) {
-    menuItems[i].addEventListener("click", function() {         
-            
-        if (this.previousElementSibling === null) {
-            this.nextElementSibling.classList.remove("tab_active");
-            this.classList.add("tab_active");
-        }
-        if (this.nextElementSibling === null) {
-            this.previousElementSibling.classList.remove("tab_active");
-            this.classList.add("tab_active");
-        }
-        if (this.previousElementSibling.classList.contains("tab_active")) {
-            this.previousElementSibling.classList.remove("tab_active");
-            this.classList.add("tab_active");
-        }
-        if (this.nextElementSibling.classList.contains("tab_active")) {
-            this.nextElementSibling.classList.remove("tab_active");
-            this.classList.add("tab_active");
-        }
-        for (let m = i; m < contentFields.length; m++) {
-            if (contentFields.item(i - 1).classList.contains("tab__content_active")) {
-                contentFields.item(i - 1).classList.remove("tab__content_active");
-                contentFields.item(m).classList.add("tab__content_active");
-            }
-            if (contentFields.item(i + 1).classList.contains("tab__content_active")) {
-                contentFields.item(i + 1).classList.remove("tab__content_active");
-                contentFields.item(m).classList.add("tab__content_active");
-            }  
-        }        
+for (let i = 0; i < menuItems.length; i++) {    
+    menuItems[i].addEventListener("click", function() {
+        for (let m = 0; m < menuItems.length; m++) {
+            menuItems[m].classList.remove("tab_active");
+            contentFields[m].classList.remove("tab__content_active");
+        };
+        menuItems[i].classList.add("tab_active");
+        contentFields[i].classList.add("tab__content_active");
     })
 }
