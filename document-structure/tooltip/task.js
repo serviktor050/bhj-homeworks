e64c107ab18a543ca1d08fed94aef9892fe32ae0
serviktor050 <mr.s-gagulin@yandex.ru>
2019-09-19 16:33:36 +0300

    Внесены корректировки в решение 1 задачи

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,14 +1,15 @@
 const answearTexts = document.getElementsByClassName("has-tooltip");
 
-for (let answearText of answearTexts) {
-    answearText.insertAdjacentHTML("afterEnd", `<div class="tooltip">${answearText.title}</div>`);
+for (let i = 0; i < answearTexts.length; i++) {
+    let placeAnswearTexts = answearTexts[i].getBoundingClientRect();
+    let answearTheQuestion = `<div class="tooltip tooltip_active" style="left:${placeAnswearTexts.x}px">${answearTexts[i].title}</div>`;
 
-    answearText.onclick = function(event) {
+    answearTexts[i].addEventListener("click", function(event) {
         event.preventDefault();
-        if(!answearText.nextElementSibling.classList.contains("tooltip_active")) {
-            answearText.nextElementSibling.classList.add("tooltip_active");
-        } else {
-            answearText.nextElementSibling.classList.remove("tooltip_active");
+        if(!event.target.nextElementSibling.classList.contains("tooltip_active")) {
+            event.target.insertAdjacentHTML("afterEnd",answearTheQuestion)
+        }else {
+            event.target.nextElementSibling.remove();
         }
-    }
+    })
 }
