f2bc39bba0ba21de256bd7e6a02f5d5246aa1ba1
serviktor050 <mr.s-gagulin@yandex.ru>
2019-09-19 12:42:13 +0300

    Внесены корректировки в решение 3 задачи

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -2,62 +2,39 @@ const products = document.querySelectorAll(".product");
 const productQuantity = document.querySelectorAll(".product__quantity-value");
 const cartWihtProducts = document.querySelector(".cart__products");
 
+function addProductCart(id, item, image) {
+  return `<div class="cart__product" data-id="${id}">
+            <img class="cart__product-image" src="${image}">
+            <div class="cart__product-count">${item}</div>
+        </div>`
+}
 
 for (let i = 0; i < products.length; i++) {
     products[i].addEventListener("click", function(event) {
         let targetItem = event.target;
+        event.preventDefault();
         if (targetItem.classList.contains("product__quantity-control_dec")) {
-            event.preventDefault();
             if (productQuantity.item(i).textContent > 1) {
                 productQuantity.item(i).textContent--;
-            }else {
-                productQuantity.textContent = 1;
-            }            
+            }             
         }
         if (targetItem.classList.contains("product__quantity-control_inc")) {
-            event.preventDefault();
             productQuantity.item(i).textContent++;     
-        }
-        const addProductFirst = function() {
-            return `<div class="cart__product" data-id="1">
-            <img class="cart__product-image" src="https://sirop.club/carabi/image/7396863/256x256/fit/">
-            <div class="cart__product-count">${productQuantity.item(i).textContent}</div>
-        </div>`
-        }
-        const addProductSecond = function() {
-            return `<div class="cart__product" data-id="2">
-            <img class="cart__product-image" src="https://cs8.pikabu.ru/post_img/2017/09/21/6/150598626812465714.jpg">
-            <div class="cart__product-count">${productQuantity.item(i).textContent}</div>
-        </div>`
         }
 
         if (targetItem.classList.contains("product__add")) {
-            event.preventDefault();                        
-            if (targetItem.closest(".product").dataset.id === "1") {                
-                if (cartWihtProducts.children.length === 0) {
-                    cartWihtProducts.innerHTML += addProductFirst();
-                }else {
-                    for (let firstchildren of cartWihtProducts.children/*let m = 0; m < cartWihtProducts.children.length; m++*/) {
-                        if (/*cartWihtProducts.children[m]*/firstchildren.dataset.id !== "1") {
-                            cartWihtProducts.innerHTML += addProductFirst();
-                        }else {
-                            console.log(1);
-                        }
-                    }               
+            let target_image = targetItem.closest(".product").querySelector('.product__image').getAttribute('src');
+            let target_id = targetItem.closest(".product").dataset.id;
+            let target_value = targetItem.closest(".product").querySelector('.product__quantity-value').textContent;    
+           
+            if (cartWihtProducts.children.length !== 0) {
+                if(cartWihtProducts.querySelector(`[data-id="${target_id}"]`)) {
+                  cartWihtProducts.querySelector(`[data-id="${target_id}"]`).querySelector('.cart__product-count').textContent = Number(cartWihtProducts.querySelector(`[data-id="${target_id}"]`).querySelector('.cart__product-count').textContent) + Number(target_value);
+                } else {
+                  cartWihtProducts.innerHTML += addProductCart(target_id, Number(target_value), target_image);
                 }              
-            }
-            if (targetItem.closest(".product").dataset.id === "2") {
-                if (cartWihtProducts.children.length === 0) {
-                    cartWihtProducts.innerHTML += addProductSecond();
-                }else {
-                    for (let k = 0; k < cartWihtProducts.children.length; k++) {
-                        if (cartWihtProducts.children[k].dataset.id !== "2") {
-                            cartWihtProducts.innerHTML += addProductSecond();
-                        }else {
-                            console.log(2);
-                        }
-                    }
-                }
+            } else {
+              cartWihtProducts.innerHTML += addProductCart(target_id, Number(target_value), target_image);
             }           
         }        
     })
