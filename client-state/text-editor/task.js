d0188e72e405fc603571dfc580c15bfb2c3f4153
serviktor050 <mr.s-gagulin@yandex.ru>
2019-09-27 14:11:01 +0300

    Внесены корректировки в 1 задачу

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -1,11 +1,14 @@
 const textArea = document.getElementById("editor");
 
-localStorage.setItem("userText", "");
-
-if (localStorage.userText !== "") {
-    textArea.value = localStorage.userText;
-}
+window.addEventListener("load", function() {
+    if (localStorage.userText !== "") {
+        textArea.value = localStorage.userText;
+    }else {
+        textArea.value = "";
+    }
+})
 
 textArea.addEventListener("input", function() {
+    localStorage.setItem("userText", "");
     localStorage.userText = textArea.value;
 })
